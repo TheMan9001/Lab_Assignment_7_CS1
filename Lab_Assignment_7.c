585986f70a2592f5569ab2b3fe47b9cb9f41e1b7
TheMan9001 <125486740+TheMan9001@users.noreply.github.com>
2023-03-22 11:30:31 -0400

    Update Lab_Assignment_7.c

--- Lab_Assignment_7.c
+++ Lab_Assignment_7.c
@@ -10,39 +10,55 @@ void printArray(int arr[], int size){
 }
 
 //bubble sort algorithm
+
+//this function sorts the array using bubble sort and prints the amount of 
+//swaps used during each run
 void bubble_Sort(int* ptr, int n){
     int temp;
-    int swaps = 0;
+    int swaps;
     //this loop stops one before the last number in the array because
     //the function compares the current number with the next number in the array
+    printf("\n");
     for (int i = 0; i < n-1; i++) {
         //(n-i)-1 is used to stop the function from rechecking numbers 
         //that are already sorted
+        
+        //sets swaps to 0 at the start of every run
+        swaps = 0;
         for (int j = 0; j < (n-i)-1; j++) {
             //if current number is greater than next number then swap numbers
             if (*(ptr + j) > *(ptr + (j+1))) { 
                 temp = *(ptr + (j+1)); 
                 *(ptr + (j+1)) = *(ptr + j); 
                 *(ptr + j) = temp; 
+                //counts the amount of swaps used
                 swaps++;
             } 
-        } 
+        }
+        //shows the amount of swaps used for each run
+        printf("Run #%i: %i\n", (i+1), swaps);
     }
-    printf("\nTotal Swaps: %i\n\n", swaps);
+    printf("\n");
 }
 
 int main(){
+    
     int numArray[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int* pointer = numArray;
     int arrLen = 9;
+    //displays current unsorted array
     printf("Current array: \n");
     printArray(pointer, arrLen);
     
-    //sorts the array and shows amount of swaps used
+    //sorts the array and shows amount of swaps used for each run
     bubble_Sort(pointer, arrLen);
     
+    //shows the now sorted array
     printf("Sorted array: \n");
     printArray(pointer, arrLen);
     
     return 0;
 }
+
+
+
